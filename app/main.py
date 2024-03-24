035b7f9277347b14cc3f3c97beefaa645ef4b34f
Balaji <mahadev.balaji@gmail.com>
2024-03-24 17:07:58 -0500

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,18 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, client_address = server_socket.accept() # wait for client
+
+    # Read data from the connection
+    data = client_socket.recv(1024)
+    # Respond with HTTP/1.1 200 OK
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+
+    # Send the response
+    client_socket.sendall(response)
+
+    # Close the connection
+    client_socket.close()
 
 
 if __name__ == "__main__":
